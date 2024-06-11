6335d95995ce8ea297b2dea6295fbd1f1bb31b4d
Lenin Compres <lenincompres@gmail.com>
2024-06-11 19:35:13 -0400

    better cards

--- samples/deck/main.js
+++ samples/deck/main.js
@@ -46,12 +46,10 @@ DOM.set({
     },
   },
 
-  style: {
-    color: palette.dark,
-    display: "flex",
-    flexDirection: "column",
-    backgroundColor: palette.base,
-  },
+  color: palette.dark,
+  display: "flex",
+  flexDirection: "column",
+  backgroundColor: palette.base,
 
   header: {
     display: "flex",
@@ -65,7 +63,6 @@ DOM.set({
   main: {
     margin: "0 auto 1em",
     width: "fit-content",
-
     menu: {
       padding: "0.5em",
       marginTop: "1em",
@@ -85,21 +82,20 @@ DOM.set({
         click: shuffle,
       }, {
         text: "Random",
+        input: {
+          id: "randomInput",
+          type: "number",
+          margin: "0 -0.8em 0 .5em",
+          width: "3em",
+          min: 1,
+          max: 10,
+          value: 1,
+          click: evt => evt.stopPropagation(),
+        },
         click: randomFlip,
       }],
-      input: {
-        id: "randomInput",
-        type: "number",
-        margin: "0 -0.8em 0 .5em",
-        width: "3em",
-        min: 1,
-        max: 10,
-        value: 1,
-      },
     },
-
     section: {
-      id: "cardDeck",
       display: "flex",
       flexWrap: "wrap",
       fontSize: "1.2em",
@@ -108,75 +104,59 @@ DOM.set({
       padding: "1em",
       maxWidth: "41em",
       minHeight: "10em",
+      content: _CARD_DECK,
     }
-
   },
 
   footer: {
     textAlign: "center",
     padding: "1em",
     p: "Created by Lenin Compres using DOM.js",
-    menu: {
-      label: "Palette: ",
-      input: {
-        id: "colorChooser",
-        type: "color",
-        change: e => DOM.set({
-          backgroundColor: e.target.value,
-        }),
-      }
-    }
   }
 
 });
 
 // Creating cards
 
-const CARD_SUITS = ["♠", "♥", "♣", "♦"];
-
 const CARD_CHARS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+const CARD_SUITS = ["♠", "♥", "♣", "♦"];
+const CARD_COLOR = {
+  "♠": " darkblue",
+  "♥": "darkred",
+  "♣": "darkgreen",
+  "♦": "darkgoldenrod"
+};
 let cards = [];
 for (suit of CARD_SUITS) {
   for (char of CARD_CHARS) {
-    let card = createNewCard(suit, char);
+    let _FLIPPED = new Binder(true);
+    card = DOM.set({
+      width: "2.5em",
+      height: "3.4em",
+      textAlign: "center",
+      padding: "1em 0.5em",
+      margin: "0.25em",
+      color: CARD_COLOR[suit],
+      backgroundColor: _FLIPPED.as(palette.accent, palette.light),
+      borderRadius: "0.25em",
+      boxShadow: "1px 1px 3px black",
+      cursor: "pointer",
+      span: {
+        opacity: _FLIPPED.as(0, 1),
+        text: char + suit,
+      },
+      click: e => card.toggle(),
+    }, "section", false);
+    card.show = () => _FLIPPED.value = true;
+    card.hide = () => _FLIPPED.value = false;
+    card.toggle = () => _FLIPPED.value ? card.hide() : card.show();
     cards.push(card);
   }
 };
+_CARD_DECK.value = cards;
 
-function createNewCard(suit, char) {
-  let card = {};
-  card._FLIPPED = new Binder(true);
-  card.show = () => card._FLIPPED.value = true;
-  card.hide = () => card._FLIPPED.value = false;
-  card.toggle = () => card._FLIPPED.value ? card.hide() : card.show();
-  card.elt = DOM.set({
-    width: "2.5em",
-    height: "3.4em",
-    textAlign: "center",
-    padding: "1em 0.5em",
-    margin: "0.25em",
-    color: (suit === "♥" || suit === "♦") ? "darkred" : "black",
-    backgroundColor: card._FLIPPED.as(palette.accent, palette.light),
-    borderRadius: "0.25em",
-    boxShadow: "1px 1px 3px black",
-    cursor: "pointer",
-    span: {
-      opacity: card._FLIPPED.as(0, 1),
-      text: char + suit,
-    },
-    click: e => card.toggle(),
-  }, "section"), false;
-  return card;
-}
 
 //Actions
-
-function addCards() {
-  for (let card of cards) {
-    cardDeck.set(card.elt);
-  }
-}
 
 function hideAll() {
   cards.forEach(card => card.hide());
@@ -192,7 +172,7 @@ function toggleAll() {
 
 function shuffle() {
   cards.sort((a, b) => Math.random() - 0.5);
-  addCards();
+  _CARD_DECK.value = cards;
 }
 
 function randomFlip() {
@@ -203,5 +183,3 @@ function randomFlip() {
   }
   shuffle();
 }
-
-addCards();
