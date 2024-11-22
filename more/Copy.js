079be1e97e181c5f1de041cbd666a9ca702cf964
Lenin Compres <lenincompres@gmail.com>
2024-11-22 18:48:44 -0500

    Update Copy.js

--- more/Copy.js
+++ more/Copy.js
@@ -1,11 +1,11 @@
 /**
  * Class that stores the copy text and retrieves the appropriate language copy (text) from a map given a key.
  * @author Lenin Compres <lenincompres@gmail.com>
- * @version 1.0.1
+ * @version 1.0.3
  * @repository https://github.com/lenincompres/DOM.js
  */
 
- class Copy {
+class Copy {
   #map = {};
   #key;
   #indexMap = {};
@@ -48,7 +48,7 @@
     let values = [];
     Object.values(this.#map).forEach(entry => Array.isArray(entry) ? entry.forEach(e => values.push(e)) : values.push(entry));
     values = values.filter(entry => typeof entry !== "string")
-    values.forEach(entry => Object.keys(entry).forEach(code => !langs.includes(code) ? langs.push(code) : null));
+    values.forEach(entry => Object.keys(entry).forEach(code => !langs.includes(code) && langs.push(code)));
     return langs.map(code => Copy.LANG[code]);
   }
 
@@ -72,25 +72,29 @@
    * @param {number} i - Index to retrieve, if that key has an array.
    * @return {string} - Text copy in the appropriate key and index.
    */
-  get(key, i = 0) {
+  get(...args) {
+    let argsType = DOM.typify(...args);
+    let key = argsType.string;
+    let i = argsType.number ? argsType.number : 0;
+    let vars = argsType.array;
     if (key === undefined) return console.error("No key was passed to the copy.");
     let val = this.#map[key];
     if (val === undefined) return console.error(`Key "${key}" not found in copy.`);
     this.#key = key;
     let lang = Copy.lang;
-    if (val[lang]) return Copy.treat(val[lang]);
-    if (typeof val === "string") return Copy.treat(val);
+    if (val[lang]) return Copy.treat(val[lang], vars);
+    if (typeof val === "string") return Copy.treat(val, vars);
     if (!Array.isArray(val)) {
       console.error(`Language "${lang}" not found in copy at "${key}".`);
-      return Copy.treat(val[Object.keys(val)[0]]);
+      return Copy.treat(val[Object.keys(val)[0]], vars);
     }
     val = val[i];
     if (val === undefined) return console.error(`No copy text found at: ${key}[${i}].`);
     this.#indexMap[key] = i;
-    if (val[lang]) return Copy.treat(val[lang]);
-    if (typeof val === "string") return Copy.treat(val);
+    if (val[lang]) return Copy.treat(val[lang], vars);
+    if (typeof val === "string") return Copy.treat(val, vars);
     console.error(`Language "${lang}" not found in copy at: ${key}[${i}].`);
-    return Copy.treat(val[Object.keys(val)[0]]);
+    return Copy.treat(val[Object.keys(val)[0]], vars);
   }
 
   /**
@@ -134,20 +138,23 @@
    * Treats the text as to add any fixes. So 
    * @param {string} str - Text to be treated.
    * @param {array} str - Texts to be treated.
+   * @param {array} vars - values to replace $0, $1, $2...
    * @return {str} - Treated text.
    * @return {array} - Array of treated texts.
    */
-  static treat(str) {
+  static treat(str, vars = []) {
     if (!str) return str;
     if (Array.isArray(str)) return str.map(i => Copy.treat(i));
-    return str.replaceAll("—", '<em class="em-dash">--</em>');
+    str = str.replaceAll("—", '<em class="em-dash">--</em>');
+    if (vars.length) vars.forEach((v, i) => str = str.replaceAll(`%${i}`, v));
+    return str;
   }
 
   /**
    * Selects the text from the given map in the appropriate language.
    * @param {object} map - A map the copy text from which to select the one in the appropriate language.
    */
-  static text = (map) => map[Copy.lang];
+  static text = (map) => Copy.treat(map[Copy.lang]);
 
   /**
    * Name to be used to saves chosen language in the local storage.
@@ -190,19 +197,22 @@
   /**
    *  This is a static instance of Copy, that can be used if you site only need one copy instance to hodl it's text.
    */
-  static copy = new Copy();
-  static get keys(){
-    return Copy.copy.keys;
+  static getDefaultInstance() {
+    if (Copy._defaultInstance) return Copy._defaultInstance;
+    return Copy._defaultInstance = new Copy();
+  }
+  static get keys() {
+    return Copy.getDefaultInstance().keys;
   }
   static get KEY() {
-    return Copy.copy.#KEY;
+    return Copy.getDefaultInstance().#KEY;
   }
-  static add = (...args) => Copy.copy.add(...args);
-  static get = (...args) => Copy.copy.get(...args);
-  static next = () => Copy.copy.next();
-  static getToggleLink = () => Copy.copy.getToggleLink();
-  static getLinkMenu = () => Copy.copy.getLinkMenu();
-  static getSelect = () => Copy.copy.getSelect();
+  static add = (...args) => Copy.getDefaultInstance().add(...args);
+  static get = (...args) => Copy.getDefaultInstance().get(...args);
+  static next = () => Copy.getDefaultInstance().next();
+  static getToggleLink = () => Copy.getDefaultInstance().getToggleLink();
+  static getLinkMenu = () => Copy.getDefaultInstance().getLinkMenu();
+  static getSelect = () => Copy.getDefaultInstance().getSelect();
 }
 
 /**
