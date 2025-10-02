e45ddff1a3f5564f2c947826a7e3abe8304d0641
Lenin Compres <lenincompres@gmail.com>
2025-10-02 09:24:11 -0400

    server

---
+++ jml-server/pages/main.js
@@ -0,0 +1,7 @@
+
+mainContent.set({
+  button: {
+    text: "Click me",
+    click: () => alert("Hello from client-side JS!")
+  }
+});
---
+++ jml-server/pages/sample.dom.js
@@ -0,0 +1,8 @@
+
+
+export default {
+  head: "head.json",
+  title: "test",
+  h1: "Hello world",
+  p: "created with DOM.js jml",
+};
